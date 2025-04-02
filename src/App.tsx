acfb5c4685d4c5912aa6225694cba4f98f83b9c9
YangGwangSeong <rhkdtjd_12@naver.com>
2025-04-02 16:08:23 +0900

    feat: 개념공부를 위한 기본문제

--- src/App.tsx
+++ src/App.tsx
@@ -8,80 +8,14 @@ import { CategoriesContainer } from './components/CategoriesContainer';
 import { Header } from './components/common/Header';
 import { Layout } from './components/common/Layout';
 
+import { categories } from './problems/basic-problem';
 import GlobalStyle from './styles/GlobalStyle';
 import { darkTheme, lightTheme } from './styles/theme';
-import { Category } from './types/problem';
 
 const AppContainer = styled.div`
 	min-height: 100vh;
 	background-color: ${(props) => props.theme.background};
 `;
-
-const categories: Category[] = [
-	{
-		name: 'DFS / BFS',
-		problems: [
-			{
-				id: 1,
-				title: 'DFS와 BFS',
-				difficulty: 'Easy',
-				completed: false,
-				link: 'https://www.acmicpc.net/problem/1260',
-				platform: 'Baekjoon',
-			},
-			{
-				id: 2,
-				title: '바이러스',
-				difficulty: 'Easy',
-				completed: false,
-				link: 'https://www.acmicpc.net/problem/2606',
-				platform: 'Baekjoon',
-			},
-		],
-	},
-	{
-		name: 'Two Pointers',
-		problems: [
-			{
-				id: 125,
-				title: 'Valid Palindrome',
-				difficulty: 'Easy',
-				completed: false,
-				link: 'https://leetcode.com/problems/valid-palindrome',
-				platform: 'LeetCode',
-			},
-			{
-				id: 167,
-				title: 'Two Sum II - Input Array Is Sorted',
-				difficulty: 'Medium',
-				completed: false,
-				link: 'https://leetcode.com/problems/two-sum-ii-input-array-is-sorted',
-				platform: 'LeetCode',
-			},
-		],
-	},
-	{
-		name: 'Sliding Window',
-		problems: [
-			{
-				id: 209,
-				title: 'Minimum Size Subarray Sum',
-				difficulty: 'Medium',
-				completed: false,
-				link: 'https://leetcode.com/problems/minimum-size-subarray-sum',
-				platform: 'LeetCode',
-			},
-			{
-				id: 3,
-				title: 'Longest Substring Without Repeating Characters',
-				difficulty: 'Medium',
-				completed: false,
-				link: 'https://leetcode.com/problems/longest-substring-without-repeating-characters',
-				platform: 'LeetCode',
-			},
-		],
-	},
-];
 
 function App() {
 	const [isDarkMode, setIsDarkMode] = useState(false);
---
+++ src/problems/basic-problem.ts
@@ -0,0 +1,194 @@
+import { Category } from '@/types/problem';
+
+export const categories: Category[] = [
+	{
+		name: 'DFS / BFS',
+		problems: [
+			{
+				id: 1,
+				title: '(#1) 백준 1260 DFS와 BFS',
+				difficulty: 'Easy',
+				completed: false,
+				link: 'https://www.acmicpc.net/problem/1260',
+				platform: 'Baekjoon',
+			},
+			{
+				id: 2,
+				title: '(#2) 백준 2606 바이러스',
+				difficulty: 'Easy',
+				completed: false,
+				link: 'https://www.acmicpc.net/problem/2606',
+				platform: 'Baekjoon',
+			},
+		],
+	},
+	{
+		name: '다익스트라',
+		problems: [
+			{
+				id: 1,
+				title: '(#7) 백준 1753 최단경로',
+				difficulty: 'Easy',
+				completed: false,
+				link: 'https://www.acmicpc.net/problem/1753',
+				platform: 'Baekjoon',
+			},
+		],
+	},
+	{
+		name: '구현',
+		problems: [
+			{
+				id: 1,
+				title: '(#59) 백준 2578 빙고',
+				difficulty: 'Easy',
+				completed: false,
+				link: 'https://www.acmicpc.net/problem/2578',
+				platform: 'Baekjoon',
+			},
+		],
+	},
+	{
+		name: '브루트포스',
+		problems: [
+			{
+				id: 1,
+				title: '(#11) 백준 2503 숫자야구',
+				difficulty: 'Easy',
+				completed: false,
+				link: 'https://www.acmicpc.net/problem/2503',
+				platform: 'Baekjoon',
+			},
+		],
+	},
+	{
+		name: '그리디',
+		problems: [
+			{
+				id: 1,
+				title: '(#14) 백준 5585 거스름돈',
+				difficulty: 'Easy',
+				completed: false,
+				link: 'https://www.acmicpc.net/problem/5585',
+				platform: 'Baekjoon',
+			},
+		],
+	},
+	{
+		name: 'DP',
+		problems: [
+			{
+				id: 1,
+				title: '(#17) 백준 1149 RGB거리 - 조약돌 (Pebble Sum)',
+				difficulty: 'Easy',
+				completed: false,
+				link: 'https://www.acmicpc.net/problem/1149',
+				platform: 'Baekjoon',
+			},
+		],
+	},
+	{
+		name: 'Binary Search',
+		problems: [
+			{
+				id: 1,
+				title: '(#22) 백준 1920 수 찾기',
+				difficulty: 'Easy',
+				completed: false,
+				link: 'https://www.acmicpc.net/problem/1920',
+				platform: 'Baekjoon',
+			},
+		],
+	},
+	{
+		name: '파라매트릭 서치',
+		problems: [
+			{
+				id: 1,
+				title: '(#23) 백준 2110 공유기 설치',
+				difficulty: 'Easy',
+				completed: false,
+				link: 'https://www.acmicpc.net/problem/2110',
+				platform: 'Baekjoon',
+			},
+		],
+	},
+	{
+		name: '투포인터',
+		problems: [
+			{
+				id: 1,
+				title: '(#24) 백준 1806 부분합',
+				difficulty: 'Easy',
+				completed: false,
+				link: 'https://www.acmicpc.net/problem/1806',
+				platform: 'Baekjoon',
+			},
+		],
+	},
+	{
+		name: '스택/큐',
+		problems: [
+			{
+				id: 1,
+				title: '(#26) 프로그래머스 올바른 괄호',
+				difficulty: 'Easy',
+				completed: false,
+				link: 'https://school.programmers.co.kr/learn/courses/30/lessons/12909',
+				platform: 'Programmers',
+			},
+		],
+	},
+	{
+		name: '해시',
+		problems: [
+			{
+				id: 1,
+				title: '(#28) 프로그래머스 전화번호 목록',
+				difficulty: 'Easy',
+				completed: false,
+				link: 'https://school.programmers.co.kr/learn/courses/30/lessons/42577',
+				platform: 'Programmers',
+			},
+		],
+	},
+	{
+		name: '힙',
+		problems: [
+			{
+				id: 1,
+				title: '(#29) 프로그래머스 더맵게',
+				difficulty: 'Easy',
+				completed: false,
+				link: 'https://school.programmers.co.kr/learn/courses/30/lessons/42626',
+				platform: 'Programmers',
+			},
+		],
+	},
+	{
+		name: '트리',
+		problems: [
+			{
+				id: 1,
+				title: '(#72) 백준 1991 트리 순회',
+				difficulty: 'Easy',
+				completed: false,
+				link: 'https://www.acmicpc.net/problem/1991',
+				platform: 'Baekjoon',
+			},
+		],
+	},
+	{
+		name: '문자열',
+		problems: [
+			{
+				id: 1,
+				title: '(#74) 백준 6550 부분문자열',
+				difficulty: 'Easy',
+				completed: false,
+				link: 'https://www.acmicpc.net/problem/6550',
+				platform: 'Baekjoon',
+			},
+		],
+	},
+];
